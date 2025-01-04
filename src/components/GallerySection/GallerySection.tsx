d24a0d45748562971a4e267f073ba8f50f8bf5cf
66HEX <hextheprod@gmail.com>
2025-01-04 02:09:52 +0100

    Update GallerySection.tsx

--- src/components/GallerySection/GallerySection.tsx
+++ src/components/GallerySection/GallerySection.tsx
@@ -56,7 +56,7 @@ const GallerySection = () => {
                         salę treningową. Mamy nadzieję, że te zdjęcia zachęcą Cię do odwiedzenia nas osobiście.
                     </p>
                 </div>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
                     {galleryImages.map((image, index) => (
                         <div
                             key={index}
